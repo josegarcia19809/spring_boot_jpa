09c8a570e0acfe40d84c02d682ad3e8d801a836f
Jose Garcia <josegarcia19809@gmail.com>
2025-02-08 17:08:04 -0600

    JPA e Hibernate: Añadiendo la clase Entity Person

---
+++ src/main/java/com/example/spring_boot_jpa/entities/Person.java
@@ -0,0 +1,68 @@
+package com.example.spring_boot_jpa.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+
+    @Column(name = "programming_language")
+    private String programmingLanguage;
+
+    public Person(Long id, String name, String lastname, String programmingLanguage) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;<
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", programmingLanguage='" + programmingLanguage + '\'' +
+                '}';
+    }
+}
