5518c8d563d0fa29571d3062da5813d0af7e674f
Jose Garcia <josegarcia19809@gmail.com>
2025-02-08 16:54:58 -0600

    JPA e Hibernate: Nuevo proyecto con JPA

---
+++ src/main/java/com/example/spring_boot_jpa/SpringBootJpaApplication.java
@@ -0,0 +1,19 @@
+package com.example.spring_boot_jpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaApplication  implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
